88abc5f52e16e7baf2c3fe54fd7a896757f0bb6c
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-11 13:51:49 -0800

    Cocktail list set up

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -2,10 +2,11 @@ import React from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
   return (
-    <div>
+    <>
       <h2>search form component</h2>
-    </div>
+    </>
   );
 };
 
