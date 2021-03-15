a51dc59d47ba24666103b8abf3b57845177d620f
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-03-14 19:01:57 -0700

    Name displaying inline

--- src/Pages/SingleCocktail.js
+++ src/Pages/SingleCocktail.js
@@ -80,6 +80,12 @@ const SingleCocktail = () => {
       <h2 className="section-title">{name}</h2>
       <div className="drink">
         <img src={img} alt={name} />
+        <div className="drink-info">
+          <p>
+            <span className="drink-data">Name: </span>
+            {name}
+          </p>
+        </div>
       </div>
     </section>
   );
