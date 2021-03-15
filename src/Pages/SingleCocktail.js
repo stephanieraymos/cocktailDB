fa9acc425fadf4bee39f8c33b0e59a06193390eb
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-03-14 20:43:44 -0500

    Drinks showing in console. useEffect is working

--- src/Pages/SingleCocktail.js
+++ src/Pages/SingleCocktail.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Loading from "../Components/Loading";
 import { useParams, Link } from "react-router-dom";
 const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
@@ -7,6 +7,21 @@ const SingleCocktail = () => {
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
   const [cocktail, setCocktail] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    async function getCocktail() {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getCocktail();
+  }, [id]);
+
   return <div></div>;
 };
 
