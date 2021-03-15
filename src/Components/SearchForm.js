06e5ae48986f64ca1819cb55bd211ceb3cf18173
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-03-14 20:24:45 -0500

    Search form

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -3,8 +3,17 @@ import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
   return (
     <>
+    <section className="section search">
+      <form action="" className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+        </div>
+      </form>
+    </section>
       <h2>search form component</h2>
     </>
   );
