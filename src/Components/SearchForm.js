e89d38d126ebecbfd1744444885b145fd23665c4
Stephanie Raymos <stephanieraymos@users.noreply.github.com>
2021-03-14 20:27:25 -0500

    Search is working

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -5,15 +5,24 @@ const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
   const searchValue = React.useRef("");
 
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
   return (
     <>
-    <section className="section search">
-      <form action="" className="search-form">
-        <div className="form-control">
-          <label htmlFor="name">Search your favorite cocktail</label>
-        </div>
-      </form>
-    </section>
+      <section className="section search">
+        <form action="" className="search-form">
+          <div className="form-control">
+            <label htmlFor="name">Search your favorite cocktail</label>
+            <input
+              type="text"
+              id="name"
+              ref={searchValue}
+              onChange={searchCocktail}
+            />
+          </div>
+        </form>
+      </section>
       <h2>search form component</h2>
     </>
   );
